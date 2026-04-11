f0d7c74b1305a84a70d75a2914be898bba72a7d5
Youhei Sakurai <sakurai.youhei@gmail.com>
2026-04-11 21:07:31 +0900

    Fix Pythonista exec query space encoding

--- internal/runtime/pythonista3.go
+++ internal/runtime/pythonista3.go
@@ -2,6 +2,8 @@ package runtime
 
 import (
 	"fmt"
+	"net/url"
+	"strings"
 )
 
 // Pythonista produces URLs for Pythonista runtimes.
@@ -13,5 +15,6 @@ type Pythonista struct {
 // QRCodeURL converts a raw script URL into a Pythonista 3 deep-link URL.
 func (p *Pythonista) QRCodeURL(publicURL string, bearerToken string) string {
 	code := fmt.Sprintf("exec(__import__(\"requests\").get(%q,headers={\"Authorization\":\"Bearer %s\"}).text)", publicURL, bearerToken)
-	return fmt.Sprintf("%s://?exec=%s", p.Scheme, code)
+	encoded := strings.ReplaceAll(url.QueryEscape(code), "+", " ")
+	return fmt.Sprintf("%s://?exec=%s", p.Scheme, encoded)
 }
