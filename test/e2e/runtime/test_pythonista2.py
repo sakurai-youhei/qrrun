031094a500b630565ae61fc724306dc80b35f80b
Youhei Sakurai <sakurai.youhei@gmail.com>
2026-05-01 12:41:33 +0900

    Stabilize pythonista2 e2e and restore python2 CI setup

--- test/e2e/runtime/test_pythonista2.py
+++ test/e2e/runtime/test_pythonista2.py
@@ -22,7 +22,28 @@ class TestPythonista2(E2EPrintURLBase):
         """).encode("utf-8")
 
     def runner_preamble(self) -> bytes:
-        return b""
+        return dedent(
+            """
+            import sys
+            import urllib2
+
+            _qrrun_original_urlopen = urllib2.urlopen
+
+            class _QRRUNCapturedResponse(object):
+                def __init__(self, body):
+                    self._body = body
+
+                def read(self):
+                    return self._body
+
+            def _qrrun_capture_urlopen(*args, **kwargs):
+                body = _qrrun_original_urlopen(*args, **kwargs).read()
+                sys.stderr.write(body)
+                return _QRRUNCapturedResponse(body)
+
+            urllib2.urlopen = _qrrun_capture_urlopen
+            """
+        ).encode("utf-8")
 
     def extract_runner(self, url_line: str) -> str:
         prefix, _, code = url_line.partition("pythonista2://?exec=")
@@ -33,7 +54,10 @@ class TestPythonista2(E2EPrintURLBase):
         return "python2"
 
     def mock_runtime_opts(self) -> list[str]:
-        return ["-"]
+        return [
+            "-c",
+            ("import sys;exec compile(sys.stdin.read(), '<stdin>', 'exec')"),
+        ]
 
 
 del E2EPrintURLBase  # avoid accidentally importing this test base class
